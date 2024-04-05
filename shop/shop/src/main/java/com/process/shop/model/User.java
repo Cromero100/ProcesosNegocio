907adc204ae54cfc41559a764aa88bd1fa7947ab
Cristian Romero <cfromerop@ufpso.edu.co>
2024-04-04 20:00:47 -0500

    Clase 4-4-2024 creacion de endpoints de user

--- shop/shop/src/main/java/com/process/shop/model/User.java
+++ shop/shop/src/main/java/com/process/shop/model/User.java
@@ -1,19 +1,17 @@
 package com.process.shop.model;
 
 import com.process.shop.model.Enum.DocumentType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Generated;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
-@Builder
+@NoArgsConstructor
 @Data
 @Entity
 public class User {
@@ -22,10 +20,11 @@ public class User {
     private Long id;
     private String fullName;
     private LocalDate birthDate;
-    private DocumentType documentTypeType;
+    private String documentType;
     private String cc;
     private String phoneNumber;
     private String email;
     private String password;
-    //private List<Address> address;
+    @OneToMany(mappedBy = "user")
+    private List<Address> address;
 }
